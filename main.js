10ac01fa162333eb5641698064720117ae22f7fa
kelvinkimotho3-coder <kelvinkimotho3@gmail.com>
2026-07-15 19:23:37 +0300

    feat: complete variable practice exercise

---
+++ main.js
@@ -0,0 +1,26 @@
+// Variable declarations
+let name = "Kelvin Kimotho";
+let age = 25;
+const birthYear = 1999;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof true);
+
+let score = 100;
+score = 150;
+
+const PI = 3.14159;
+
+// Exercise: Variable Practice
+let myName = "Kelvin Kimotho";
+let myAge = 22;
+let isStudent = true;
+let favoriteColors = ["blue", "green", "black"];
+let today = new Date();
+
+console.log("My name is:", myName);
+console.log("My age is:", myAge);
+console.log("Am I a student?", isStudent);
+console.log("My favorite colors are:", favoriteColors);
+console.log("Today's date is:", today);
